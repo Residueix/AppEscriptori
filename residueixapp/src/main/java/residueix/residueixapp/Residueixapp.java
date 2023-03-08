920baeaa45d6a751c710f0ae5825b1cab81d945d
isard <isard@DESKTOP-P0UA02U>
2023-03-08 13:41:10 +0100

    Càrrega inicial

---
+++ residueixapp/src/main/java/residueix/residueixapp/Residueixapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package residueix.residueixapp;
+
+/**
+ *
+ * @author isard
+ */
+public class Residueixapp {
+
+    public static void main(String[] args) {
+        System.out.println("Joper!");
+    }
+}
